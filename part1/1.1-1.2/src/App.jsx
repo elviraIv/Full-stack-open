07fa62fbc37b2d2c562756045fdf046c2083b1be
Elvira <elvira.n.ivanova@gmail.com>
2023-11-26 16:06:43 +0200

    Update

--- part1/1.1-1.2/src/App.jsx
+++ part1/1.1-1.2/src/App.jsx
@@ -1,70 +1,22 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
-
-const Content = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}) => {
-  return (
-    <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </div>
-  );
-};
-
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
-};
+import { useState } from 'react'
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const [ counter, setCounter ] = useState(0)
+
+  const handleClick = () => setCounter(counter + 1)
+  const resetHandler = () => setCounter(0)
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={parts[0].name}
-        exercises1={parts[0].exercises}
-        part2={parts[1].name}
-        exercises2={parts[1].exercises}
-        part3={parts[2].name}
-        exercises3={parts[2].exercises}
-      />
+    <div>{counter}</div>
+    <button onClick={handleClick}> 
+      plus
+    </button>
+    <button onClick={resetHandler}> 
+      reset
+    </button>
+  </div>
+  )
+}
 
-      <Total
-        exercises1={parts[0].exercises}
-        exercises2={parts[1].exercises}
-        exercises3={parts[2].exercises}
-      />
-    </div>
-  );
-};
-
-export default App;
+export default App
