e935d3f46e47bd3cb2da4f4d1bb4b9244e47c7a3
Elvira <elvira.n.ivanova@gmail.com>
2023-11-28 14:22:37 +0200

    Add solution to 2.3*: Course information step8

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,11 +2,16 @@ import Content from "./Content";
 import Header from "./Header";
 
 const Course = ({ course }) => {
-  console.log({ course });
+  let allExercises = course.parts.reduce(
+    (sum, current) => sum + current.exercises,
+    0
+  );
+
   return (
     <>
       <Header title={course.name} />
       <Content key={course.id} course={course} />
+      <p>total of {allExercises} exercises</p>
     </>
   );
 };
