2f59401b34675b346a2f27a03ca01fb92f122063
Elvira <elvira.n.ivanova@gmail.com>
2023-11-28 14:46:13 +0200

    Add solution to 2.4: Course information step9

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,12 +1,21 @@
+import Header from "./Header";
 import Part from "./Part";
 
 const Content = ({ course }) => {
+  let allExercises = course.parts.reduce(
+    (sum, current) => sum + current.exercises,
+    0
+  );
   return (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
+    <>
+      <Header title={course.name} />
+      <div>
+        {course.parts.map((part) => (
+          <Part key={part.id} part={part} />
+        ))}
+      </div>
+      <p style={{ fontWeight: "bold" }}>total of {allExercises} exercises </p>
+    </>
   );
 };
 
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,17 +1,11 @@
 import Content from "./Content";
-import Header from "./Header";
 
-const Course = ({ course }) => {
-  let allExercises = course.parts.reduce(
-    (sum, current) => sum + current.exercises,
-    0
-  );
-
+const Course = ({ courses }) => {
   return (
     <>
-      <Header title={course.name} />
-      <Content key={course.id} course={course} />
-      <p>total of {allExercises} exercises</p>
+      {courses.map((course) => (
+        <Content key={course.id} course={course} />
+      ))}
     </>
   );
 };
