6216a2cfa45edb786907d8713859433be7e1f5a7
Elvira <elvira.n.ivanova@gmail.com>
2023-11-29 15:02:56 +0200

    Add solution to 2.15*: The Phonebook step10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -30,8 +30,23 @@ const App = () => {
       number: newNumebr,
     };
 
-    if (persons.find((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+    const existingEntry = persons.find((person) => person.name === newName);
+
+    console.log(existingEntry);
+    if (existingEntry) {
+      window.confirm(
+        `${newName} is already added to phonebookreplace the old number with a new one?`
+      );
+      contactService
+        .update(existingEntry.id, noteObj)
+        .then((returnedPerson) => {
+          setPersons(
+            persons.map((person) =>
+              person.id !== returnedPerson.id ? person : returnedPerson
+            )
+          );
+        });
+
       setNewName("");
       setNewNumebr("");
       return;
@@ -46,22 +61,20 @@ const App = () => {
   };
 
   const deleteHandler = (id) => {
-    const person = persons.find((p) => p.id === id)
-   
-    if(window.confirm(`Delete ${person.name}?`))
-    contactService.deleteContact(id).then(() => {
-      contactService.getAll().then((persons) => {
-        setPersons(persons)
-      })
-    });
+    const person = persons.find((p) => p.id === id);
 
-   
+    if (window.confirm(`Delete ${person.name}?`))
+      contactService.deleteContact(id).then(() => {
+        contactService.getAll().then((persons) => {
+          setPersons(persons);
+        });
+      });
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter setFilter={setFilter}/>
+      <Filter setFilter={setFilter} />
       <h3>Add a new</h3>
       <PersonForm
         nameValue={newName}
@@ -69,10 +82,13 @@ const App = () => {
         numberValue={newNumebr}
         onChangeNumber={numberChangeHandler}
         onClick={submitNameHandler}
-        
       />
       <h3>Numbers</h3>
-      <Persons persons={persons} filter={filter} deleteHandler={deleteHandler} />
+      <Persons
+        persons={persons}
+        filter={filter}
+        deleteHandler={deleteHandler}
+      />
     </div>
   );
 };
