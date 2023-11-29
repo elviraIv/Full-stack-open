a0a6dcab7a77f4a358f50fc314aab1327e760d44
Elvira <elvira.n.ivanova@gmail.com>
2023-11-29 08:32:28 +0200

    Fetch data from server

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note");
   const [showAll, setShowAll] = useState(false);
+
+  useEffect(()=> {
+    console.log('effect')
+    axios
+          .get('http://localhost:3000/notes')
+          .then(response => {
+            console.log('promise fulfilled');
+            setNotes(response.data)
+          })
+  },[])
+
+  console.log('render', notes.length, 'notes');
 
   const onChangeHandler = (event) => {
     console.log(event.target.value);
@@ -24,7 +37,7 @@ const App = (props) => {
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
-
+ 
   return (
     <div>
       <h1>Notes</h1>
