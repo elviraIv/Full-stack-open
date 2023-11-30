9835b4194e9147cc5b354e1a503dc8ca11321b5f
Elvira <elvira.n.ivanova@gmail.com>
2023-11-30 09:28:55 +0200

    Add solution to 2.19*: Data for countries, step2

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,10 +1,12 @@
-const Countries = ({ countriesToShow }) => {
+const Countries = ({ countriesToShow, setCountriesToShow}) => {
   if (countriesToShow.length === 1) return null;
 
   return (
     <div>
       {countriesToShow.map((country) => (
-        <div key={country.name.common}>{country.name.common}</div>
+        <div key={country.name.common}>{country.name.common}
+        <button onClick={() => setCountriesToShow([country])}>show</button>
+        </div>
       ))}
     </div>
   );
