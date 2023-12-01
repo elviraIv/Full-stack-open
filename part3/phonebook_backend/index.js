bb97090f3d34a55f8b6df5926b49c43a77f6028a
Elvira <elvira.n.ivanova@gmail.com>
2023-12-01 18:45:48 +0200

    Add solution to 3.18*: Phonebook database step6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -30,10 +30,9 @@ const Person = require("./models/person");
 const errorHandler = (error, request, response, next) => {
   console.log(error.message);
   if (error.name === "CastError") {
-    return response.status(400).send({error:'malformatted id'})
+    return response.status(400).send({ error: "malformatted id" });
   }
-
-}
+};
 app.use(express.json());
 app.use(morgan("tiny"));
 
@@ -55,29 +54,36 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}<p>`
-  );
+app.get("/info", (request, response, next) => {
+  Person.find({})
+    .then((people) => {
+      response.send(
+        `<p>Phonebook has info for ${
+          people.length
+        } people</p><p>${new Date()}<p>`
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (!person) {
-    response.status(404).end();
-  }
-
-  response.json(person);
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((errror) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
-      response.status(204).end()
+      response.status(204).end();
     })
-    .catch((error) => next(error))
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -97,6 +103,21 @@ app.post("/api/persons", (request, response) => {
   person.save().then((savedPerson) => {
     response.json(savedPerson);
   });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+
+  const person = {
+    name: name,
+    number: number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
