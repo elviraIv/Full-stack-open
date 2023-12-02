680fbdbb3a9988aedfbc978622d962c2fedb9ae9
Elvira <elvira.n.ivanova@gmail.com>
2023-12-02 13:28:06 +0200

    Add solution to 3.20*: Phonebook database, step8

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -1,37 +1,59 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
+const url = process.env.MONGODB_URI;
 
-const url = process.env.MONGODB_URI
+console.log("connecting to", url);
 
+mongoose
+  .connect(url)
 
-console.log('connecting to', url)
-
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const numberValidators = [
+  {
+    // Minimal length & proper dash validator
+    validator: (number) => {
+      if ((number[2] === "-" || number[3] === "-") && number.length < 9) {
+        return false;
+      }
+      return true;
+    },
+    msg: "must be at least 8 digits",
+  },
+  {
+    validator: (number) => {
+      return /^\d{2,3}-\d+$/.test(number);
+    },
+    msg: "ivalid phone format",
+  },
+];
 
 const personSchema = new mongoose.Schema({
   name: {
-    type:String,
-    minLength:3
+    type: String,
+    minLength: 3,
+    required: true,
   },
-  number: String,
-})
+  number: {
+    type: String,
+    required: true,
+    validate: numberValidators,
+  },
+});
 
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema);
