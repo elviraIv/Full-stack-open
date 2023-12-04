5b4b993293c16608f4afb49bef4e67e87b7f3cb0
Elvira <elvira.n.ivanova@gmail.com>
2023-12-04 09:37:56 +0200

    Add solution to 4.2 Blog list, step2

---
+++ part4/blog_list_app/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config');
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+require("dotenv").config();
+const logger = require('./utils/logger');
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+   logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
--- part4/blog_list_app/index.js
+++ part4/blog_list_app/index.js
@@ -1,52 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require("dotenv").config();
-const config = require('./utils/config');
-
-
-app.use(express.json());
-app.use(cors());
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
