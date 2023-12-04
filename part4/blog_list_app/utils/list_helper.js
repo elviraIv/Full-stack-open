95b45087e912d7cd21106d3352dae7c0a09c2c15
Elvira <elvira.n.ivanova@gmail.com>
2023-12-04 11:32:27 +0200

    Add soluton to 4.5*:  unit tests, step3

--- part4/blog_list_app/utils/list_helper.js
+++ part4/blog_list_app/utils/list_helper.js
@@ -1,18 +1,37 @@
 const dummy = (blogs) => {
-    return 1
-  }
+  return 1;
+};
 
-  const totalLikes = (blogs) => {
-      let allLikes = 0;
-    blogs.forEach((blog) => {
-        
-        allLikes += blog.likes
+const totalLikes = (blogs) => {
+    
+  let allLikes = 0;
+  blogs.forEach((blog) => {
+    allLikes += blog.likes;
+  });
+  return allLikes;
+};
 
-    })
-    return allLikes
-  }
-  
-  module.exports = {
-    dummy,
-    totalLikes
-  }
+const favouriteBlog = (blogs) => {
+    if(blogs.length === 0) return null
+  return Math.max(...blogs.map((blog) => blog.likes));
+};
+const favouriteBlogArticle = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const mostLikedBlog = blogs.reduce((prevValue, currentValue) => {
+    return prevValue.likes > currentValue.likes ? prevValue : currentValue;
+  });
+
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes,
+  };
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favouriteBlog,
+  favouriteBlogArticle,
+};
