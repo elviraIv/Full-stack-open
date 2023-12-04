7e32b04c99cf4660b7b2d62c1d43ad047e1bc737
Elvira <elvira.n.ivanova@gmail.com>
2023-12-04 10:41:56 +0200

    Add solution to 4.4:  unit tests, step2

--- part4/blog_list_app/utils/list_helper.js
+++ part4/blog_list_app/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+      let allLikes = 0;
+    blogs.forEach((blog) => {
+        
+        allLikes += blog.likes
+
+    })
+    return allLikes
+  }
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
