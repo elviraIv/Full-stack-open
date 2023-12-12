76dfb5398c98e23451de8372b2d6b6fd375cd3e8
Elvira <elvira.n.ivanova@gmail.com>
2023-12-12 20:21:21 +0200

    Add solution 6.13 Better anecdotes, step11

--- part6/anecdotes_redux/src/components/AnecdoteList.jsx
+++ part6/anecdotes_redux/src/components/AnecdoteList.jsx
@@ -1,11 +1,13 @@
 import { useSelector, useDispatch } from "react-redux";
 import { addVote } from "../reducers/anecdoteReducer";
+import { setNotification } from "../reducers/notificationReducer";
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) => state.anecdotes);
   const dispatch = useDispatch();
 
-  const vote = (id) => {
-    dispatch(addVote(id));
+  const vote = (anecdote) => {
+    dispatch(addVote(anecdote.id));
+    dispatch(setNotification(`You voted ${anecdote.content}`))
     
     
   };
@@ -18,7 +20,7 @@ const AnecdoteList = () => {
             <div>{anecdote.content}</div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
+              <button onClick={() => vote(anecdote)}>vote</button>
             </div>
           </div>
         ))}
