b290f8b3da0d44a022ed6a3ed715f86361a495f4
Elvira <elvira.n.ivanova@gmail.com>
2023-12-12 17:24:31 +0200

    Add visibility filter

--- part6/notes_redux/src/App.jsx
+++ part6/notes_redux/src/App.jsx
@@ -1,66 +1,27 @@
-import NewNote from './components/NewNote'
-import Notes from './components/Notes'
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
+import VisibilityFilter from "./components/VisibilityFilter";
+import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(state => state)
+  const dispatch = useDispatch();
+  const notes = useSelector((state) => state);
 
   const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    dispatch(createNote(content))
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    dispatch(createNote(content));
+  };
 
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id))
-  }
-
-  return(
+  return (
     <div>
-     <NewNote />
-     <Notes />
+      <NewNote />
+      <Notes />
+      <VisibilityFilter />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default App;
