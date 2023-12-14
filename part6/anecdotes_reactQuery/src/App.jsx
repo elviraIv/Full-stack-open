a335ec6bb90b424fec5202c5cbd3fb7b3e202fb8
Elvira <elvira.n.ivanova@gmail.com>
2023-12-14 11:00:14 +0200

    Initial code

--- part6/anecdotes_reactQuery/src/App.jsx
+++ part6/anecdotes_reactQuery/src/App.jsx
@@ -1,7 +1,6 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
 import { getAnecdotes, updateAnecdote } from "./requests";
 
 const App = () => {
