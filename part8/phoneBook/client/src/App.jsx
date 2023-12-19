4f224938603acd781598a1490952964370aab4a7
Elvira <elvira.n.ivanova@gmail.com>
2023-12-19 16:20:15 +0200

    Update

--- part8/phoneBook/client/src/App.jsx
+++ part8/phoneBook/client/src/App.jsx
@@ -1,26 +1,48 @@
-import { useState } from "react";
-import { useQuery } from "@apollo/client";
-import Persons from "./components/Persons";
-import PersonForm from "./components/PersonForm";
-import Notify from "./components/Notify";
-import PhoneForm from "./components/PhoneForm";
-import LoginForm from "./components/LoginForm";
-import { ALL_PERSONS } from "./queries";
+import { useQuery, useApolloClient, useSubscription } from '@apollo/client'
+import { useState } from 'react'
 
-const App = () => {
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [token, setToken] = useState(null)
-  const result = useQuery(ALL_PERSONS);
-  const client = useApolloClient()
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import PhoneForm from './components/PhoneForm'
+import Notify from './components/Notify'
+import LoginForm from './components/LoginForm'
 
-  if (result.loading) {
-    return <div>loading...</div>;
+import { ALL_PERSONS, PERSON_ADDED } from './queries'
+
+export const updateCache = (cache, query, addedPerson) => {
+  // helper that is used to eliminate saving same person twice
+  const uniqByName = (a) => {
+    let seen = new Set()
+    return a.filter((item) => {
+      let k = item.name
+      return seen.has(k) ? false : seen.add(k)
+    })
   }
 
-  const logout = () => {
-    setToken(null)
-    localStorage.clear()
-    client.resetStore()
+  cache.updateQuery(query, ({ allPersons }) => {
+    return {
+      allPersons: uniqByName(allPersons.concat(addedPerson)),
+    }
+  })
+}
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  useSubscription(PERSON_ADDED, {
+    onData: ({ data }) => {
+      const addedPerson = data.data.personAdded
+      notify(`${addedPerson.name} added`)
+
+      updateCache(client.cache, { query: ALL_PERSONS }, addedPerson)
+    }
+  })
+
+  if (result.loading)  {
+    return <div>loading...</div>
   }
 
   const notify = (message) => {
@@ -30,28 +52,31 @@ const App = () => {
     }, 10000)
   }
 
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
   if (!token) {
     return (
-      <div>
+      <>
         <Notify errorMessage={errorMessage} />
-        <h2>Login</h2>
-        <LoginForm
-          setToken={setToken}
-          setError={notify}
-        />
-      </div>
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
     )
   }
 
   return (
     <div>
-    <Notify errorMessage={errorMessage} />
-    <button onClick={logout}>logout</button>
-    <Persons persons = {result.data.allPersons} />
-    <PersonForm setError={notify} />
-    <PhoneForm />
-  </div>
-  );
-};
+      <Notify errorMessage={errorMessage} />
+      <PersonForm setError={notify} />
+      <button onClick={logout}>logout</button>
+      <Persons persons = {result.data.allPersons} />
 
-export default App;
+      <PhoneForm  setError={notify} />
+    </div>
+  )
+}
+
+export default App
--- part8/phoneBook/client/src/queries.js
+++ part8/phoneBook/client/src/queries.js
@@ -1,57 +1,59 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client'
 
 const PERSON_DETAILS = gql`
   fragment PersonDetails on Person {
     id
     name
-    phone
+    phone 
     address {
-      street
+      street 
       city
     }
   }
-`;
+`
 
 export const ALL_PERSONS = gql`
   query {
-    allPersons {
+    allPersons  {
       name
       phone
       id
     }
   }
-`;
+`
+
+export const CREATE_PERSON = gql`
+  mutation createPerson($name: String!, $street: String!, $city: String!, $phone: String) {
+    addPerson(
+      name: $name,
+      street: $street,
+      city: $city,
+      phone: $phone
+    ) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      ...PERSON_DETAILS
+      ...PersonDetails
     }
   }
-  ${PERSON_DETAILS}
-`;
 
-export const CREATE_PERSON = gql`
-  mutation createPerson(
-    $name: String!
-    $street: String!
-    $city: String!
-    $phone: String
-  ) {
-    addPerson(name: $name, street: $street, city: $city, phone: $phone) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
-    }
-  }
-`;
+  ${PERSON_DETAILS}
+`
 
 export const EDIT_NUMBER = gql`
   mutation editNumber($name: String!, $phone: String!) {
-    editNumber(name: $name, phone: $phone) {
+    editNumber(name: $name, phone: $phone)  {
       name
       phone
       address {
@@ -61,12 +63,22 @@ export const EDIT_NUMBER = gql`
       id
     }
   }
-`;
+`
 
 export const LOGIN = gql`
   mutation login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
+    login(username: $username, password: $password)  {
       value
     }
   }
-`;
+`
+
+export const PERSON_ADDED = gql`
+  subscription {
+    personAdded {
+      ...PersonDetails
+    }
+  }
+  
+${PERSON_DETAILS}
+`
