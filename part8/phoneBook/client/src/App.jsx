0b36520126b475dab65b0182d3b2bf1d4b04b2db
Elvira <elvira.n.ivanova@gmail.com>
2023-12-19 08:44:58 +0200

    Update

---
+++ part8/phoneBook/client/src/App.jsx
@@ -0,0 +1,33 @@
+import { useQuery } from "@apollo/client";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import { ALL_PERSONS } from "./queries";
+import Notify from "./components/Notify";
+import { useState } from "react";
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  return (
+    <div>
+    <Notify errorMessage={errorMessage} />
+    <Persons persons = {result.data.allPersons} />
+    <PersonForm setError={notify} />
+  </div>
+  );
+};
+
+export default App;
